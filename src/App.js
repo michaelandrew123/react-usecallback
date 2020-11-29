97628a89c42454f233f6e87d93713b45e172ebe9
Michael Andrew Suarez <michaelandrewsuarez0@gmail.com>
2020-11-29 11:22:25 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from 'react'
+import List from './List'
 
 function App() {
+  const [number, setNumber] = useState(1)
+  const [dark, setDark] = useState(false)
+
+  const getItems = useCallback((incrementer) => {
+
+    return [number+incrementer, number + 1 + incrementer, number+2 + incrementer]
+  }, [number])
+  
+
+  const theme = { 
+    backgroundColor: dark ? '#333' : '#fff',
+    color: dark ? '#fff' : '#333'
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={theme}>
+        <input type='number' value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+
+        <button onClick={() => setDark(prevDark => !prevDark)}>
+          Toggle Theme
+        </button>
+        <List getItems={getItems} />
+
+      </div>
+
+      <h1>Hello Call Back Function</h1>
+    </>
   );
 }
 
